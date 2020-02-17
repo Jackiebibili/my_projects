b77b96ed812e8049491ddcd2c3d7d0c743b2c732
Jackiebibili <61096708+Jackiebibili@users.noreply.github.com>
2020-02-16 19:53:47 -0600

    a new program A=LDU added

---
+++ Factorization_A=LDU/exeMatrixFactorization.cpp
@@ -0,0 +1,8 @@
+/*
+This program is to compute the factorization of
+matrix A into a lower triangular matrix L and the
+upper triagular matrix U. In order to record the
+array of pivots, the matrix U is divided into a
+diagonal matrix D and U with 1's on the main diagonal
+entries.
+*/
